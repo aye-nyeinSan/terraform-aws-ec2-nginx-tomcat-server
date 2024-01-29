cde0d594171d1be72e22ed1a406e1bf88be08e61
Aye Nyein San <aye2904@gmail.com>
2024-01-29 20:51:33 +0700

    install nginx

--- securitygroup.tf
+++ securitygroup.tf
@@ -24,6 +24,15 @@ resource "aws_security_group" "my_sg" {
     cidr_blocks      = ["0.0.0.0/0"]
 
   }
+   
+
+   egress {
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+    }
 
   tags = {
     Name = "my_security_group"
